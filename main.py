788858489f24a819480efa5b3e93a7f66492c84d
alistairw2601 <65949679+alistairw2601@users.noreply.github.com>
2023-08-03 16:18:21 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import shutil
+import discord
+from discord.ext import commands, tasks
+from itertools import cycle
+import os
+import asyncio
+import random
+import os
+
+client = commands.Bot(command_prefix="=", intents=discord.Intents.all())
+
+bot_status = cycle(["Hello","use the prefix =","=help for commands"])
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+f=open("BotToken.txt",mode='r')
+token=f.read()
+f.close()
+
+@tasks.loop(seconds=3)
+async def status_change():
+    await client.change_presence(activity=discord.Game(next(bot_status)))
+
+
+@client.event
+async def on_ready():
+    print("Bot is connected")
+    status_change.start()
+
+@client.event
+async def on_message(message):
+    if message.author.id == client.user.id:
+        return
+
+    if "=dice" in message.content:
+        await  message.channel.send(f"You rolled {random.randint(1,6)}!")
+
+client.run(token)
